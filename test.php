d592511fd95833546e17a4cab9bff68331d972ed
Guiliassembly <raynerwijaya841@gmail.com>
2024-10-01 15:03:34 +0700

    test php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+    $test_array = array();
+    $test_array[0]=1;
+    $test_array[1]=2;
+
+
+    //print array
+    echo "hi there";
+    for($i=0;count($test_array);$i++){
+        echo $test_array[$i],"\n";
+        
+    }
+
+    
+    
+?>
